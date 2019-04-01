d92ee1ff2c61fd85b77c28402e8393a990bfe751
Samuel Overton Jr <samovertonjr@gmail.com>
2019-03-31 23:16:31 -0500

    poke-api created

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log(res);
+
+  res.status(401).send({
+    errors: [{ status: 401, title: 'Not Found', detail: 'The requested uri was not found.' }]
+  });
+});
+
+app.get('/pokemon', (req, res) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/?limit=150`;
+  axios
+    .get(url)
+    .then((response) => {
+      const pokemon = response.data.results.sort(function(a, b) {
+        return a.name.localeCompare(b.name);
+      });
+      res.send(pokemon);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Error has occurred');
+    });
+});
+
+app.get('/pokemon/:id', (req, res) => {
+  const id = req.params.id;
+
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
+  axios
+    .get(url)
+    .then((response) => {
+      const { height, weight } = response.data;
+      const pokeImage = response.data.sprites.front_default;
+      const moves = response.data.moves.map((move) => {
+        return move['move'].name;
+      });
+      const types = response.data.types.map((type) => {
+        return type['type'].name;
+      });
+
+      res.send({
+        results: {
+          height,
+          weight,
+          moves,
+          types,
+          pokeImage
+        }
+      });
+    })
+
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send('Error has occurred');
+    });
+});
+
+app.listen('8000', () => {
+  console.log('listening');
+});
