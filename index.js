121263557305cec40c976070fc1d3942ff80585e
Samuel Overton Jr <samovertonjr@gmail.com>
2019-04-02 04:41:48 -0500

    added allow cors for api

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const axios = require('axios');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/', (req, res) => {
   console.log(res);
 
